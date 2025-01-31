c2248737c9461eb11ca3f2212daf7bfb11c63be0
Tamir <jtamir846@gmail.com>
2025-02-01 00:38:11 +0800

    hha

---
+++ app.js
@@ -0,0 +1,18 @@
+// Player's turn
+var activePlayer = 0;
+
+// Players turn
+var scores = [0, 0];
+
+// Player's round score
+var roundScore = 2;
+//<p>Current Score: <span id="current1">0</span></p>
+
+var dice = Math.floor(Math.random() * 6) + 1;
+document.getElementById("#current1").textContent = 0;
+document.getElementById("#total1").textContent = 0;
+document.getElementById("#current2").textContent = 0;
+document.getElementById("#total2").textContent = 0;
+// document.querySelector("#current2").innerHTML = "<em>dice<em>";
+// document.querySelector(".dice").style.display = "none";
+console.log(dice);
