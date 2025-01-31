e5a52e07c8c6807e6bf8153b0025c6565a8a6e66
Tamir <jtamir846@gmail.com>
2025-02-01 00:49:22 +0800

    hha

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ var roundScore = 2;
 //<p>Current Score: <span id="current1">0</span></p>
 
 var dice = Math.floor(Math.random() * 6) + 1;
-document.getElementById("#current1").textContent = 0;
-document.getElementById("#total1").textContent = 0;
-document.getElementById("#current2").textContent = 0;
-document.getElementById("#total2").textContent = 0;
+document.getElementById("current1").textContent = 0;
+document.getElementById("total1").textContent = 0;
+document.getElementById("current2").textContent = 0;
+document.getElementById("total2").textContent = 0;
 // document.querySelector("#current2").innerHTML = "<em>dice<em>";
 // document.querySelector(".dice").style.display = "none";
-console.log(dice);
+// console.log(dice);
+document.querySelector(".rollButton").addEventListener("click", shooShid);
+
+function shooShid() {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  if (dice !== 1) alert(dice + " is good");
+  else alert(dice + " oh no");
+}
